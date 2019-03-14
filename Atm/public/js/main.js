e565645fd24bf3b06d211b1a52a4e5c051a08e91
logeeqw <43319952+logeeqw@users.noreply.github.com>
2019-03-14 10:16:58 -0400

    Add files via upload

---
+++ Atm/public/js/main.js
@@ -0,0 +1,19 @@
+/**
+ *@author Quinn Logee
+ *@version 0.01
+ * @summary rolls an input type of dice an input number of time
+ * @todo nothing
+ */
+
+"use strict";
+const PROMPT = require(`readline-sync`);
+
+let deposit, withdraw, transfer;
+
+function main() {
+
+}
+
+main();
+
+document.getElementById(get);
